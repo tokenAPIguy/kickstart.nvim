7198d339ee8437bcaa866c31afe004304f379af8
tokenAPIguy <token.api.guy@gmail.com>
2025-04-12 23:51:39 -0400

    added config - cht.sh

--- init.lua
+++ init.lua
@@ -1004,22 +1004,45 @@ vim.keymap.set('n', '<C-b>', '<C-b>zz')
 -- Add fold
 vim.api.nvim_set_keymap('n', '<leader>cz', [[/[{[(]<CR>zfaBzc]], { noremap = true, silent = true, desc = 'Add fold at next {, (, or [' })
 
--- Open Terminal
-vim.keymap.set('n', '<leader>0', function()
+-- Open Mini Terminal
+vim.keymap.set('n', '<leader>tm', function()
   vim.cmd.vnew()
   vim.cmd.term()
   vim.cmd.wincmd 'J'
   vim.cmd 'startinsert'
   vim.api.nvim_win_set_height(0, 10)
-end)
+end, { desc = '[T]erminal [M]ini' })
 
--- Open Lazygit
-vim.keymap.set('n', '<leader>lg', function()
+-- Open Lazygit Terminal
+vim.keymap.set('n', '<leader>tlg', function()
   vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd 'startinsert'
   vim.cmd 'terminal lazygit'
-end)
+  vim.cmd 'startinsert'
+end, { desc = '[T]erminal [L]azy[g]it' })
+
+-- Open cht.sh/go
+vim.keymap.set('n', '<leader>tcg', function()
+  -- Prompt the user for input
+  vim.ui.input({ prompt = 'cht.sh query (e.g., go/reverse string): ' }, function(input)
+    if input and input ~= '' then
+      -- Escape input for curl (e.g., spaces)
+      local query = input:gsub(' ', '+')
+
+      -- Open vertical split and run curl command
+      vim.cmd.vnew()
+      vim.cmd('terminal curl cht.sh/go/' .. query .. '; exec $SHELL')
+      vim.cmd 'startinsert'
+    end
+  end)
+end, { desc = '[T]erminal [C]ht.sh/[G]o' })
+
+-- Open cht.sh/go
+vim.keymap.set('n', '<leader>tcv', function()
+  -- Prompt the user for input
+  vim.cmd.vnew()
+  vim.cmd 'terminal curl cht.sh/vim; exec $SHELL'
+  vim.cmd 'startinsert'
+end, { desc = '[T]erminal [C]ht.sh/[V]im' })
 
 -- Custom Plugins
 vim.o.termguicolors = true
