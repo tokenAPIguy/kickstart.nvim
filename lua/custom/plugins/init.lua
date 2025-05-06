5d86b54f275693ee66709d5fd5dd87ac8ea40c45
tokenAPIguy <token.api.guy@gmail.com>
2025-05-05 22:04:46 -0400

    added oil-nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,9 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
 return {
   { import = 'custom.plugins.remote.smear_cursor' },
   { import = 'custom.plugins.remote.startup-nvim' },
+  { import = 'custom.plugins.remote.oil-nvim' },
 
   { import = 'custom.plugins.local.floaterminal' },
   { import = 'custom.plugins.local.toggle_comments' },
